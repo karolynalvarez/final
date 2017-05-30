3010385202691c581b29bfdb2714a4aae2584bc8
karolynalvarez <kari_a97@yahoo.com>
2017-05-30 16:32:12 -0700

    added item collection and student collection

--- libraryManage/libraryManage/Library.cpp
+++ libraryManage/libraryManage/Library.cpp
@@ -11,6 +11,16 @@ Library::~Library()
 {
 }
 
+void Library::addStudent(std::shared_ptr<Student> _student)
+{
+	allStudents.push_back(_student);
+}
+
+void Library::addItem(std::shared_ptr<Item> _item)
+{
+	allItems.push_back(_item);
+}
+
 void Library::printBookList()
 {
 }
--- libraryManage/libraryManage/Library.h
+++ libraryManage/libraryManage/Library.h
@@ -2,15 +2,18 @@
 #include <memory>
 #include <string>
 #include "Student.h"
+#include "Item.h"
 class Library
 {
 private:
 	std::string locationName;
 	std::vector<std::shared_ptr<Student>> allStudents;
-	std::vector<std::shared_ptr<Student>> allBooks;
+	std::vector<std::shared_ptr<Item>> allItems;
 public:
 	Library(std::string);
 	~Library();
+	void addStudent(std::shared_ptr<Student>);
+	void addItem(std::shared_ptr<Item>);
 	void printBookList();
 };
 
